d5cd8a7d8754bdc307b9840d795fb14c9b21fd14
Maddy Alletru <maddy.alletru2022@campus-eni.fr>
2023-04-14 11:40:21 +0200

    modif cahier de commandes

---
+++ src/components/Clients.vue
@@ -0,0 +1,39 @@
+<template>
+  <table border>
+    <tr>
+        <th>Nom</th>
+        <th>Prénom</th>
+        <th>Téléphone</th>
+        <th>Adresse</th>
+        <th>Code Postal</th>
+        <th>Ville</th> 
+    </tr>
+    <tr v-for="(customer,index) in list" :key="index">
+        <td>{{ customer.lastname }}</td>
+        <td>{{ customer.firstname }}</td>   
+        <td>{{ customer.phoneNumber }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+
+export default {
+    name: 'CliEnts',
+    props: ['list']
+}
+</script>
+
+<style>
+    th {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 120%;
+        text-align: center;
+        background-color: rgb(39, 173, 173);
+    }
+
+    td{
+        font-size: 100%;
+        text-align: center;
+    }
+</style>
