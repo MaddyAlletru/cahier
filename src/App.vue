73004394be990a95dca80cf9e181dfed6733e851
Maddy Alletru <maddy.alletru2022@campus-eni.fr>
2023-04-14 11:39:45 +0200

    création cahier de commandes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="newOrder">Nouvelle commande</button>
+  <h1>Cahier de commandes</h1>
+  <span>Aujourd'hui, nous sommes le {{ date }} et il est {{ time }}</span>  
+
+  <h2>Afficher les commandes du : 
+    <input type="date" @click="showList"><br>
+    <span>Voici les commandes du jour</span> 
+    <orders :listOrders="orders"></orders>
+  </h2>
+
+  <h2>Liste des clients</h2>
+  <clients :list="customers"></clients>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clients from './components/Clients.vue'
+import Orders from './components/Orders.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Clients,
+    Orders,
+  }, 
+  data(){
+    return{
+        customers: [
+          {firstname: "Maddy", lastname: "Alletru", phoneNumber: "0000000000"},
+          {firstname: "Valentin", lastname: "Bailleul", phoneNumber: "0000000000"}, 
+          {firstname: "Julie", lastname: "Corbec"},
+          {lastname: "Peiniau", phoneNumber: "0000000000"},
+        ], 
+        
+        date: new Date().toLocaleDateString(),
+        time: new Date().toLocaleTimeString(),
+
+        orders: [
+          {orderNumber: "001", clientName: 'Client1', clientPhone: "0000000000", description: "blablablabbablablalblbalba"}, 
+          {orderNumber: "002", clientName: 'Client2', clientPhone: "0000000000", description: "blablablabbablablalblbalba"}, 
+          {orderNumber: "003", clientName: 'Client3', clientPhone: "0000000000", description: "blablablabbablablalblbalba"},
+        ]
+    }
+  }, 
+  methods:{
+    showList(){
+      alert('test')
+    }, 
+    newOrder(){
+      alert('test')
+    }
   }
 }
 </script>
---
+++ src/components/Orders.vue
@@ -0,0 +1,20 @@
+<template>
+  <span v-for="(order,index) in listOrders" :key="index">
+    {{ order.orderNumber }}
+    {{ order.clientName }}
+    {{ order.clientPhone }}
+    {{ order.description }} 
+</span>
+</template>
+
+<script>
+
+export default {
+    name: 'OrdeRs',
+    props: '[listOrders]'
+}
+</script>
+
+<style>
+
+</style>
